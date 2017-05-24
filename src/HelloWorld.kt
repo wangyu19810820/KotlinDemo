e8de68ca5305e591edb1ec50ae3c8a2dd18a35f3
wangyu <123456>
2017-05-25 00:38:58 +0800

    a

---
+++ src/HelloWorld.kt
@@ -0,0 +1,10 @@
+/**
+ * Created by admin on 2017/5/24.
+ */
+fun main(args:Array<String>) {
+    if (args.size == 0) {
+        println("Please enter a args")
+        return
+    }
+    println("Hello World: ${args[0]}!");
+}
